c9e6bd9d0d44c77a816008a90a5fdd9a0e02a817
Dominik Kobinski <dominikkobinski314@gmail.com>
2024-08-14 20:19:37 +0200

    Build: Initial boot.img

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [ $1 == 'OneM8' ]; then
+    # TODO: Add dtb
     cat BootShim/BootShim.bin workspace/Build/HtcOneM8/DEBUG_GCC/FV/MSM8974_UEFI.fd >>ImageResources/HtcOneM8/bootpayload.bin
 
-    #mkbootimg --kernel ImageResources/HtcOneM8/bootpayload.bin --base 0x11800000 --kernel_offset 0x00008000 -o ImageResources/HtcOneM8/uefi.img
+    # --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
+    mkbootimg --kernel ImageResources/HtcOneM8/bootpayload.bin --base 0x00000000 --kernel_offset 0x00008000 -o ImageResources/HtcOneM8/uefi.img
 else
     echo "Bootimages: Invalid platform"
 fi
