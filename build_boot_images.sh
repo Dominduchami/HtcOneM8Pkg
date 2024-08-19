0821c4c4c375f257ac4ea1cad5ba18ea32a9d3ae
Dominik Kobinski <dominikkobinski314@gmail.com>
2024-08-19 14:10:30 +0200

    M8: Add dtb

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 if [ $1 == 'OneM8' ]; then
-    # TODO: Add dtb
     cat BootShim/BootShim.bin workspace/Build/HtcOneM8/DEBUG_GCC/FV/MSM8974_UEFI.fd >>ImageResources/HtcOneM8/bootpayload.bin
 
-    # --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
+    dtc -I dts -O dtb ImageResources/HtcOneM8/msm8974-htc-m8.dts -o ImageResources/HtcOneM8/msm8974-htc-m8.dtb
+
+    cat ImageResources/HtcOneM8/msm8974-htc-m8.dtb >>ImageResources/HtcOneM8/bootpayload.bin
+
+    # --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
     mkbootimg --kernel ImageResources/HtcOneM8/bootpayload.bin --base 0x00000000 --kernel_offset 0x00008000 -o ImageResources/HtcOneM8/uefi.img
 else
     echo "Bootimages: Invalid platform"
