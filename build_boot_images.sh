b8b1aaf1bb4048336133a5c8890bc29e1a12f078
Dominduchami <dominikkobinski314@gmail.com>
2025-01-10 19:24:49 +0100

    Build: Update and switch to clang

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-if [ $1 == 'OneM8' ]; then
-    cat BootShim/BootShim.bin workspace/Build/HtcOneM8/DEBUG_GCC/FV/MSM8974_UEFI.fd >>ImageResources/HtcOneM8/bootpayload.bin
+cat BootShim/BootShim.bin workspace/Build/HtcOneM8/DEBUG_CLANGDWARF/FV/MSM8974_UEFI.fd >>ImageResources/HtcOneM8/bootpayload.bin
 
-    dtc -I dts -O dtb ImageResources/HtcOneM8/msm8974-htc-m8.dts -o ImageResources/HtcOneM8/msm8974-htc-m8.dtb
+dtc -I dts -O dtb ImageResources/HtcOneM8/msm8974-htc-m8.dts -o ImageResources/HtcOneM8/msm8974-htc-m8.dtb
 
-    cat ImageResources/HtcOneM8/msm8974-htc-m8.dtb >>ImageResources/HtcOneM8/bootpayload.bin
+cat ImageResources/HtcOneM8/msm8974-htc-m8.dtb >>ImageResources/HtcOneM8/bootpayload.bin
 
-    # --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
-    mkbootimg --kernel ImageResources/HtcOneM8/bootpayload.bin --base 0x00000000 --kernel_offset 0x00008000 -o ImageResources/HtcOneM8/uefi.img
-else
-    echo "Bootimages: Invalid platform"
-fi
+# --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
+mkbootimg --kernel ImageResources/HtcOneM8/bootpayload.bin --base 0x00000000 --kernel_offset 0x00008000 -o ImageResources/HtcOneM8/uefi.img
